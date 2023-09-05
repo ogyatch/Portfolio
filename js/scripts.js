ba964b1b082c8c660dd08ed1ce7afcf1e31e3441
ogyatch <selves.aglow_0k@icloud.com>
2023-09-05 18:59:56 +0900

    閉じるアイコンを最上位に

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
-$(document).ready(function(){
-    $('#hamburger-menu').click(function(){
+$(document).ready(function () {
+    $('#hamburger-menu').click(function () {
         $('#menu-shade').toggle(); // シェードの表示・非表示を切り替え
-        
+
         const currentIconSrc = $('#menu-icon').attr('src');
-        
-        if(currentIconSrc === 'img/bars-solid.png') {
+
+        if (currentIconSrc === 'img/bars-solid.png') {
             $('#menu-icon').attr('src', 'img/xmark-solid.png');
+            $('.hamburger-menu').css('z-index', '1001'); // シェードより上に
         } else {
             $('#menu-icon').attr('src', 'img/bars-solid.png');
+            $('.hamburger-menu').css('z-index', '0'); // 元に戻す
         }
     });
 
-    $('#menu-shade').click(function(){
+    $('#menu-shade').click(function () {
         $(this).hide();
         $('#menu-icon').attr('src', 'img/bars-solid.png');
+        $('.hamburger-menu').css('z-index', '0'); // 元に戻す
     });
 });
