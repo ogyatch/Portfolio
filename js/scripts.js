8a6f56132a3e4f0746b56e42e4df4908bd27a66f
ogyatch <selves.aglow_0k@icloud.com>
2023-09-05 13:07:06 +0900

    ハンバーガーメニュー仮実装完

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.getElementById('menu-icon');
-    const overlay = document.getElementById('overlay');
-
-    menuIcon.addEventListener('click', function() {
-        if (overlay.style.display === 'none' || overlay.style.display === '') {
-            overlay.style.display = 'flex';
-            menuIcon.src = 'img/xmark-solid.png';
+$(document).ready(function(){
+    $('#hamburger-menu').click(function(){
+        $('#menu-shade').toggle(); // シェードの表示・非表示を切り替え
+        
+        const currentIconSrc = $('#menu-icon').attr('src');
+        
+        if(currentIconSrc === 'img/bars-solid.png') {
+            $('#menu-icon').attr('src', 'img/xmark-solid.png');
         } else {
-            overlay.style.display = 'none';
-            menuIcon.src = 'img/bars-solid.png';
+            $('#menu-icon').attr('src', 'img/bars-solid.png');
         }
+    });
+
+    $('#menu-shade').click(function(){
+        $(this).hide();
+        $('#menu-icon').attr('src', 'img/bars-solid.png');
     });
 });
