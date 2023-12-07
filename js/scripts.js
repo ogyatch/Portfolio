8cce2ffa0254b409f7200117b1d734727179bf0a
200700031 <tomohiro.ogawa.yb@kyocera.jp>
2023-12-07 10:13:25 +0900

    リストアニメーション後にクリック可能に

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // サムネイルの処理
-    $('.thumbnail').hover(function(){
+    $('.thumbnail').hover(function () {
         // オリジナルのテキストを保存
         let originalTextCategory = $(this).find('.thumbnail-category').text();
         let originalTextTitle = $(this).find('.thumbnail-title').text();
@@ -9,31 +9,43 @@ $(document).ready(function(){
         animateText($(this).find('.thumbnail-category'), originalTextCategory, 340);
         animateText($(this).find('.thumbnail-title'), originalTextTitle, 340);
 
-    }, function(){
+    }, function () {
         // マウスが離れたときに元のテキストに戻す
         $(this).find('.thumbnail-category').text($(this).find('.thumbnail-category').attr('data-original-text'));
         $(this).find('.thumbnail-title').text($(this).find('.thumbnail-title').attr('data-original-text'));
     });
 
-    // リスト項目の処理
-    $('ul li').each(function(){
-        // 各リスト項目のオリジナルのテキストを保存
+    // $('ul li').each(function () {
+    //     $(this).attr('data-original-text', $(this).text());
+    // }).hover(function () {
+    //     let originalText = $(this).attr('data-original-text');
+    //     animateText($(this), originalText, 340);
+    // }, function () {
+    //     $(this).text($(this).attr('data-original-text'));
+    // });
+
+    // リンク付きリスト項目の処理
+    $('ul li[data-url]').each(function(){
         $(this).attr('data-original-text', $(this).text());
     }).hover(function(){
-        // マウスオーバーされたときの処理
         let originalText = $(this).attr('data-original-text');
-        animateText($(this), originalText, 340);
+        animateText($(this), originalText, 340, () => {
+            // アニメーション終了後の処理
+            $(this).addClass('clickable').on('click', function() {
+                window.open($(this).data('url'), '_blank');
+            });
+        });
     }, function(){
         // マウスが離れたときの処理
+        $(this).removeClass('clickable').off('click');
         $(this).text($(this).attr('data-original-text'));
     });
 });
 
-function animateText(element, originalText, duration){
+function animateText(element, originalText, duration, callback){
     let length = originalText.length;
     let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-[]{}|;:',.<>?/";
 
-    // テキストをクリアする
     element.text("".repeat(length));
 
     for (let i = 0; i < length; i++) {
@@ -44,13 +56,16 @@ function animateText(element, originalText, duration){
                 element.text(tempText.substring(0, i) + randomChar + tempText.substring(i + 1));
             }, 50);
 
-            // アニメーションを終了し、オリジナルの文字に戻す
             setTimeout(function(){
                 clearInterval(charAnimation);
                 let tempText = element.text();
                 element.text(tempText.substring(0, i) + originalText.charAt(i) + tempText.substring(i + 1));
+
+                if (i === length - 1 && callback) {
+                    callback();
+                }
             }, 340);
 
-        }, i * (550 / length)); // 各文字に対するタイムラグを設定
+        }, i * (550 / length));
     }
 }
