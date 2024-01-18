1e29e90d17ab0ab1c1c0ddd937d4fbdee35e68fe
200700031 <tomohiro.ogawa.yb@kyocera.jp>
2024-01-19 08:13:28 +0900

    add resume.html

---
+++ js/resume.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+    var password = prompt("Please enter the password to view this page:", '');
+    if (password != 'YourPassword') {
+        window.location.href = 'index.html'
+    }
+</script>
