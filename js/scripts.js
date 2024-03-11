a8ec744049fd7512b4450f806500b17bfe3abdd0
200700031 <tomohiro.ogawa.yb@kyocera.jp>
2024-03-11 14:33:48 +0900

    ヘッダーを共通化

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,27 @@
 $(document).ready(function () {
-    // サムネイルの処理
+
     $('.thumbnail').hover(function () {
-        // オリジナルのテキストを保存
         let originalTextCategory = $(this).find('.thumbnail-category').text();
         let originalTextTitle = $(this).find('.thumbnail-title').text();
 
-        // アニメーションを開始
         animateText($(this).find('.thumbnail-category'), originalTextCategory, 340);
         animateText($(this).find('.thumbnail-title'), originalTextTitle, 340);
 
     }, function () {
-        // マウスが離れたときに元のテキストに戻す
         $(this).find('.thumbnail-category').text($(this).find('.thumbnail-category').attr('data-original-text'));
         $(this).find('.thumbnail-title').text($(this).find('.thumbnail-title').attr('data-original-text'));
     });
 
-    // $('ul li').each(function () {
-    //     $(this).attr('data-original-text', $(this).text());
-    // }).hover(function () {
-    //     let originalText = $(this).attr('data-original-text');
-    //     animateText($(this), originalText, 340);
-    // }, function () {
-    //     $(this).text($(this).attr('data-original-text'));
-    // });
-
-    // リンク付きリスト項目の処理
     $('ul li[data-url]').each(function(){
         $(this).attr('data-original-text', $(this).text());
     }).hover(function(){
         let originalText = $(this).attr('data-original-text');
         animateText($(this), originalText, 340, () => {
-            // アニメーション終了後の処理
             $(this).addClass('clickable').on('click', function() {
                 window.open($(this).data('url'), '_blank');
             });
         });
     }, function(){
-        // マウスが離れたときの処理
         $(this).removeClass('clickable').off('click');
         $(this).text($(this).attr('data-original-text'));
     });
